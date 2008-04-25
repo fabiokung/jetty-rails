e924fa46df95a0319c476b915905eeaa1362fc38
Fabio Kung <fabio.kung@gmail.com>
2008-04-25 17:57:00 -0300

    required jars organization

--- lib/jetty_rails.rb
+++ lib/jetty_rails.rb
@@ -2,7 +2,8 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 require "java"
-require "jetty_rails/jetty_jars"
+require "jruby-rack-1.0-SNAPSHOT"
+require "jetty_rails/jars"
 require "jetty_rails/runner"
 
 module JettyRails
