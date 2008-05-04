8ac9d244cfea83d5d3fa927593bfe233c413e6c7
Fabio Kung <fabio.kung@gmail.com>
2008-05-04 15:38:56 -0300

    specs for command line options

--- spec/jetty_rails_spec.rb
+++ spec/jetty_rails_spec.rb
@@ -13,6 +13,24 @@ describe "binary executable with no command line arguments" do
     load File.dirname(__FILE__) + '/../bin/jetty_rails'
   end
   
+  it "should not set the context path by default" do
+    runner = mock("runner", :null_object => true)
+    JettyRails::Runner.should_receive(:new) do |config|
+      config.should_not have_key(:context_path)
+      runner
+    end
+    load File.dirname(__FILE__) + '/../bin/jetty_rails'
+  end
+  
+  it "should not set the environment by default" do
+    runner = mock("runner", :null_object => true)
+    JettyRails::Runner.should_receive(:new) do |config|
+      config.should_not have_key(:environment)
+      runner
+    end
+    load File.dirname(__FILE__) + '/../bin/jetty_rails'
+  end
+  
 end
 
 describe "binary executable with command line arguments" do
@@ -23,6 +41,30 @@ describe "binary executable with command line arguments" do
     JettyRails::Runner.should_receive(:new) do |config|
       config.should have_key(:base)
       config[:base].should eql('/any/app/dir')
+      runner
+    end
+    load File.dirname(__FILE__) + '/../bin/jetty_rails'
+  end
+  
+  it "should take the second command line argument as context path" do
+    ARGV[0] = '/any/app/dir'
+    ARGV[1] = '/myapp'
+    runner = mock("runner", :null_object => true)
+    JettyRails::Runner.should_receive(:new) do |config|
+      config.should have_key(:context_path)
+      config[:context_path].should eql('/myapp')
+      runner
+    end
+    load File.dirname(__FILE__) + '/../bin/jetty_rails'
+  end
+  
+  it "should take the third command line argument as custom environment" do
+    ARGV[0] = '/any/app/dir'
+    ARGV[2] = 'production'
+    runner = mock("runner", :null_object => true)
+    JettyRails::Runner.should_receive(:new) do |config|
+      config.should have_key(:environment)
+      config[:environment].should eql('production')
       runner
     end
     load File.dirname(__FILE__) + '/../bin/jetty_rails'
