8ba2486964ba22d39564a95a4dc9daaeceec735d
Fabio Kung <fabio.kung@gmail.com>
2008-05-04 12:03:28 -0300

    using activesupport

--- lib/jetty_rails.rb
+++ lib/jetty_rails.rb
@@ -2,6 +2,8 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 require "java"
+require "rubygems"
+require "activesupport"
 require "jetty_rails/jars"
 require "jetty_rails/runner"
 
