562368dfad0c23cd18fa3169beb79ef1dbd83f91
Fabio Kung <fabio.kung@gmail.com>
2008-06-10 16:07:39 -0300

    todos

--- lib/jetty_rails/warbler_reader.rb
+++ lib/jetty_rails/warbler_reader.rb
@@ -2,10 +2,12 @@ class WarblerReader
   attr_reader :config
   
   def initialize
+    # TODO ignore jruby and jruby-rack
     config = load("#{config[:base]}/config/warble.rb")
     config.java_libs.each do |jar|
       require jar
     end
+    # TODO require custom classes
   end
   
   
