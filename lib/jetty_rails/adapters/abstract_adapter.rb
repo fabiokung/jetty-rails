ceb66abb600abc6dab3624c8321f2b70206cad9c
Jeffrey Damick <jdamick@Macintosh-92.local>
2008-06-11 09:51:59 -0400

    ported over from branch

---
+++ lib/jetty_rails/adapters/abstract_adapter.rb
@@ -0,0 +1,24 @@
+module JettyRails
+  module Adapters
+    class AbstractAdapter
+      attr_reader :config
+      
+      def initialize(config)
+        @config = config
+      end
+      
+      def base_init_params()
+        @base_init_params ||= { 
+          'public.root' => '/public',
+          'gem.path' => config[:gem_path] || ENV['GEM_PATH'] || 'tmp/war/WEB-INF/gems',
+          'jruby.initial.runtimes' => "#{config[:jruby_initial_runtimes]}",
+          'jruby.max.runtimes' => "#{config[:jruby_max_runtimes]}"
+        }
+      end
+      
+      def event_listeners
+        []
+      end
+    end
+  end
+end
