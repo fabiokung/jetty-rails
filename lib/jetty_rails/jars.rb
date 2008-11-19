f28bf3833482e3023331de1075a943fe917efcc3
Fabio Kung <fabio.kung@gmail.com>
2008-11-19 06:13:18 -0200

    upgrading jetty to 6.1.14

--- lib/jetty_rails/jars.rb
+++ lib/jetty_rails/jars.rb
@@ -1,9 +1,9 @@
 module JettyRails
   
-  require "servlet-api-2.5-6.1.11"
-  require "jetty-6.1.11"
-  require "jetty-util-6.1.11"
-  require "jetty-plus-6.1.11"
+  require "servlet-api-2.5-6.1.14"
+  require "jetty-6.1.14"
+  require "jetty-util-6.1.14"
+  require "jetty-plus-6.1.14"
   require "core-3.1.1"
   require "jsp-api-2.1"
   require "jsp-2.1"
