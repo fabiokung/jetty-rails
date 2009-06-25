53ead4a17efe6c38e35bdb487b6d0d2ff874daaa
fabiokung <fabio.kung@gmail.com>
2009-06-24 23:51:39 -0300

    updating newgem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,12 @@
 begin
   require 'spec'
 rescue LoadError
-  require 'rubygems'
+  require 'rubygems' unless ENV['NO_RUBYGEMS']
   gem 'rspec'
   require 'spec'
 end
 
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 $:.unshift(File.dirname(__FILE__) + '/../jetty-libs')
-require 'jetty_rails'
+require 'jetty-rails'
 require 'yaml'
